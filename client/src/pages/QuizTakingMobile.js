ae387e7c71f0baf7250b090602da4c8faf751b9a
m-qu-le-md <mqule.md.hmu@gmail.com>
2026-07-05 23:16:04 +0700

    fix: resolve ESLint exhaustive-deps warning in QuizTakingMobile.js

--- client/src/pages/QuizTakingMobile.js
+++ client/src/pages/QuizTakingMobile.js
@@ -27,6 +27,7 @@ const QuizTakingMobile = ({
   const currentQuestion = displayQuestions?.[currentQuestionIndex];
 
   // Tạo bộ màu thay đổi mỗi khi người dùng vuốt sang câu hỏi đơn mới
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const randomColors = useMemo(() => {
     return [...PASTEL_VARIANTS].sort(() => 0.5 - Math.random());
   }, [currentQuestionIndex]);
